5b4b2cb3003df09b007888c416f1510c7637c3c6
Nick Scavelli <nick@vndly.com>
2022-08-30 11:33:21 -0400

    Backport logging busy/active gauges to 19.10.0

--- gunicorn/arbiter.py
+++ gunicorn/arbiter.py
@@ -554,14 +554,18 @@ class Arbiter(object):
             self.kill_worker(pid, signal.SIGTERM)
 
         busy_workers = sum([ 1 for worker in workers if worker[1].busy.value ])
+        self.log.debug("{0} busy workers".format(busy_workers),
+                       extra={"metric": "gunicorn.busy_workers",
+                              "value": busy_workers,
+                              "mtype": "gauge"})
 
         active_worker_count = len(workers)
         if self._last_logged_active_worker_count != active_worker_count:
             self._last_logged_active_worker_count = active_worker_count
-            self.log.debug("{0} workers".format(active_worker_count))
-
-        self.log.gauge("gunicorn.workers", active_worker_count)
-        self.log.gauge("gunicorn.busy_workers", busy_workers)
+            self.log.debug("{0} workers".format(active_worker_count),
+                           extra={"metric": "gunicorn.workers",
+                                  "value": active_worker_count,
+                                  "mtype": "gauge"})
 
     def spawn_worker(self):
         self.worker_age += 1
